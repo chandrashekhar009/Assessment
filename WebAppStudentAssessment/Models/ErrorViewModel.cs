e4b6325d48c3a3dbfb3d2f7b8e0bc3146a4edc35
chandrashekhar sharma <chandrashekhar.s@coforgetech.com>
2020-12-04 18:20:02 +0545

    first commit

---
+++ WebAppStudentAssessment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppStudentAssessment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
